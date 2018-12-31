b06aedf062ee4a19be4eda2987cf24d422939359
shawn a meighn <smeighn@gmail.com>
2018-12-31 23:19:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,8 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
-}
+  return parseInt(n, 10) + two
 
 var funkyFunction = function() {
   return function() {
